e9470797388d18dde9b52fa767b53f12a6e5b5fb
SoohyeonLee <soohyeon.lee@kakao.com>
2022-01-19 00:17:45 +0900

    [feat] AlbumCell UI 구성

--- SimpleMusicPlayer/SimpleMusicPlayer/View/MediaCollectionViewCell.swift
+++ SimpleMusicPlayer/SimpleMusicPlayer/View/MediaCollectionViewCell.swift
@@ -7,6 +7,75 @@
 
 import UIKit
 
+import SnapKit
+
 class MediaCollectionViewCell: UICollectionViewCell {
-    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.configure()
+    }
+
+    // MARK: Internal
+    func setAlbum(_ album: MediaAlbum) {
+        self.artworkImageView.image = album.artwork?.image(
+            at: CGSize(width: self.bounds.width, height: self.bounds.width)
+        )
+    }
+
+    // MARK: UI Property
+    private lazy var artworkImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    private lazy var albumTitleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = .preferredFont(forTextStyle: .title2)
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
+    private lazy var albumArtistLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray
+        label.font = .preferredFont(forTextStyle: .body)
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
+
+    // MARK: Life Cycle Function
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.artworkImageView.image = nil
+        self.albumTitleLabel.text = nil
+        self.albumArtistLabel.text = nil
+    }
+
+    // MARK: Class Method
+    private func configure() {
+        self.drawUI()
+    }
+
+    private func drawUI() {
+        self.addSubview(self.artworkImageView)
+        self.artworkImageView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.height.equalTo(self.artworkImageView.snp.width)
+        }
+        self.addSubview(self.albumTitleLabel)
+        self.albumTitleLabel.snp.makeConstraints {
+            $0.top.equalTo(self.artworkImageView.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview().inset(8)
+        }
+        self.addSubview(self.albumArtistLabel)
+        self.albumArtistLabel.snp.makeConstraints {
+            $0.top.equalTo(self.artworkImageView.snp.bottom).offset(4)
+            $0.leading.trailing.bottom.equalToSuperview().inset(8)
+        }
+    }
 }
