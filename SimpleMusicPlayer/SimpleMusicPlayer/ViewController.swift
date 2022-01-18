5f612aff8c271b77fbbfa81c29d73345e3839612
SoohyeonLee <soohyeon.lee@kakao.com>
2022-01-18 20:42:27 +0900

    [config] Xcode 프로젝트 생성

---
+++ SimpleMusicPlayer/SimpleMusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleMusicPlayer
+//
+//  Created by Soohyeon Lee on 2022/01/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
